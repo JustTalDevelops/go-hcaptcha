59f8926d587e5d05f58cc4d3ae9758a35a59ef4e
Tal <justtal@protonmail.com>
2021-03-18 22:14:08 -0500

    Add documentation to HSW

--- hsw.go
+++ hsw.go
@@ -7,7 +7,12 @@ import (
 	"net/http"
 )
 
-func getHsw(host, sitekey, userAgent string, page playwright.Page) (hsw string, original string, err error) {
+// getHsw sends a request to the HCaptcha site config system for a HSW token. 
+// Then, we use the original token provided to us to generate HSW to send in our captcha requests.
+// The HSW is generated through Playwright. On initial startup, Playwright (running Firefox, WebKit seems to fail)
+// opens a new empty tab, injects the HSW script by HCaptcha, and then evaluates the HSW using the page evaluate
+// function. It then returns the response, plus the HSW token and an error in case anything went wrong.
+func getHsw(host, sitekey, userAgent string, page playwright.Page) (hsw string, token string, err error) {
 	req, err := http.NewRequest("GET", "https://hcaptcha.com/checksiteconfig?host="+host+"&sitekey="+sitekey+"&sc=1&swa=1", nil)
 	if err != nil {
 		return "", "", err

89b5f21659eb54b8997aaf27bc758b57d3af7152
JustTal <justtal@protonmail.com>
2021-03-19 17:55:51 -0500

    Lot's of changes, solver now works and implemented HSW pools & workers.
    HSW pools and workers haven't been integrated in yet, but the next commit will do just that.

--- hsw.go
+++ hsw.go
@@ -1,35 +1,179 @@
 package main
 
 import (
+	"errors"
+	"github.com/corpix/uarand"
 	"github.com/mxschmitt/playwright-go"
+	"github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
 	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
-// getHsw sends a request to the HCaptcha site config system for a HSW token. 
+// HSW is an HSW token, containing both the C and N values used for requests to HCaptcha.
+type HSW struct {
+	// C and N are tokens used by HCaptcha on the getcaptcha endpoint as a form of authorization.
+	// These can not be easily spoofed, as they contain browser/device level data.
+	C, N string
+}
+
+// HSWWorker is a worker for an HSW pool.
+// Workers should always be created through the NewWorker function.
+type HSWWorker struct {
+	playwrightInstance *playwright.Playwright
+	playwrightBrowser  playwright.Browser
+	playwrightPage     playwright.Page
+	pool               *HSWPool
+	running            bool
+}
+
+// Close closes all Playwright instances, and set's the running status to false to close HSW generation.
+func (h *HSWWorker) Close() {
+	h.playwrightPage.Close()
+	h.playwrightBrowser.Close()
+	h.playwrightInstance.Stop()
+
+	h.running = false
+}
+
+// NewWorker returns a new HSW worker to be used in HSW pools.
+func NewWorker(pool *HSWPool) (*HSWWorker, error) {
+	var err error
+	worker := &HSWWorker{pool: pool, running: true}
+
+	worker.playwrightInstance, err = playwright.Run()
+	if err != nil {
+		return nil, err
+	}
+	worker.playwrightBrowser, err = worker.playwrightInstance.Firefox.Launch()
+	if err != nil {
+		return nil, err
+	}
+	worker.playwrightPage, err = worker.playwrightBrowser.NewPage()
+	if err != nil {
+		return nil, err
+	}
+	_, err = worker.playwrightPage.AddScriptTag(playwright.PageAddScriptTagOptions{Url: pool.hswScriptUrl})
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		var hsw HSW
+		for {
+			hsw, err = generateHsw(pool.host, pool.siteKey, worker.playwrightPage)
+
+			// We run the check after the HSW has been generated,
+			// because we are more likely to be already generating HSW
+			// when a shutdown is requested.
+			if !worker.running {
+				break
+			}
+			if err != nil {
+				pool.log.Error(err)
+				continue
+			}
+			pool.hswPoolMutex.Lock()
+			pool.hswPool = append(pool.hswPool, hsw)
+			pool.hswPoolMutex.Unlock()
+		}
+	}()
+
+	return worker, nil
+}
+
+// HSWPool is a pool of HSW tokens that get regenerated on the fly by HSW workers.
+// These are used to generate valid captcha codes.
+type HSWPool struct {
+	hswPool       []HSW
+	hswPoolMutex  *sync.Mutex
+	workers       []*HSWWorker
+	hswScriptUrl  *string
+	host, siteKey string
+	log           *logrus.Logger
+}
+
+// NewHSWPool creates a new HSW pool with the amount of workers specified.
+func NewHSWPool(host, siteKey, hswScriptUrl string, log *logrus.Logger, workers int) (pool *HSWPool, err error) {
+	if workers <= 0 {
+		return nil, errors.New("invalid worker amount for pool")
+	}
+	pool = &HSWPool{
+		hswPoolMutex: &sync.Mutex{},
+		hswScriptUrl: playwright.String(hswScriptUrl),
+		host:         host,
+		siteKey:      siteKey,
+		log:          log,
+	}
+
+	var worker *HSWWorker
+	for i := 0; i < workers; i++ {
+		worker, err = NewWorker(pool)
+		if err != nil {
+			return nil, err
+		}
+		pool.workers = append(pool.workers, worker)
+	}
+
+	return
+}
+
+// ActiveWorkers returns true if there are any active workers.
+func (h *HSWPool) ActiveWorkers() (active bool) {
+	for _, w := range h.workers {
+		if w.running {
+			active = true
+		}
+	}
+
+	return
+}
+
+// GetHSW gets a new HSW token from the HSW pool.
+func (h *HSWPool) GetHSW() (HSW, error) {
+	for {
+		if !h.ActiveWorkers() {
+			return HSW{}, errors.New("no active workers found")
+		}
+		if len(h.hswPool) != 0 {
+			h.hswPoolMutex.Lock()
+			defer func() {
+				h.hswPool[len(h.hswPool)-1], h.hswPool[0] = h.hswPool[0], h.hswPool[len(h.hswPool)-1]
+				h.hswPool = h.hswPool[:len(h.hswPool)-1]
+				h.hswPoolMutex.Unlock()
+			}()
+			return h.hswPool[0], nil
+		}
+	}
+}
+
+// generateHsw sends a request to the HCaptcha site config system for a HSW token.
 // Then, we use the original token provided to us to generate HSW to send in our captcha requests.
 // The HSW is generated through Playwright. On initial startup, Playwright (running Firefox, WebKit seems to fail)
 // opens a new empty tab, injects the HSW script by HCaptcha, and then evaluates the HSW using the page evaluate
 // function. It then returns the response, plus the HSW token and an error in case anything went wrong.
-func getHsw(host, sitekey, userAgent string, page playwright.Page) (hsw string, token string, err error) {
-	req, err := http.NewRequest("GET", "https://hcaptcha.com/checksiteconfig?host="+host+"&sitekey="+sitekey+"&sc=1&swa=1", nil)
+func generateHsw(host, siteKey string, page playwright.Page) (hsw HSW, err error) {
+	req, err := http.NewRequest("GET", "https://hcaptcha.com/checksiteconfig?host="+host+"&siteKey="+siteKey+"&sc=1&swa=1", nil)
 	if err != nil {
-		return "", "", err
+		return HSW{}, err
 	}
-	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("User-Agent", uarand.GetRandom())
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", "", err
+		return HSW{}, err
 	}
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", "", err
+		return HSW{}, err
 	}
 	result := gjson.ParseBytes(b)
 	pResp, err := page.Evaluate("hsw(\"" + result.Get("c.req").String() + "\");")
 	if err != nil {
-		return "", "", err
+		return HSW{}, err
 	}
-	return pResp.(string), result.Get("c").String(), nil
+	return HSW{
+		C: result.Get("c").String(),
+		N: pResp.(string),
+	}, nil
 }
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/corpix/uarand"
-	"github.com/google/go-querystring/query"
 	"github.com/mxschmitt/playwright-go"
 	"github.com/tidwall/gjson"
 	"io/ioutil"
@@ -22,8 +21,9 @@ type Task struct {
 	Key   string `json:"task_key"`
 }
 
+var sRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	pw, err := playwright.Run()
 	if err != nil {
 		panic(err)
@@ -36,107 +36,52 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	page.AddScriptTag(playwright.PageAddScriptTagOptions{URL: playwright.String("https://assets.hcaptcha.com/c/6043b6da/hsw.js")})
-	fmt.Println(tryToSolve("minecraftpocket-servers.com", "e6b7bb01-42ff-4114-9245-3d2b7842ed92", page))
+	page.AddScriptTag(playwright.PageAddScriptTagOptions{Url: playwright.String("https://assets.hcaptcha.com/c/6043b6da/hsw.js")})
+
+	for {
+		code, err := tryToSolve("cd252234-44d8-4b81-b1d5-d4e14b624834", "minecraftpocket-servers.com", page)
+		if err != nil {
+			continue
+		}
+		fmt.Println(code)
+		break
+	}
+
+	page.Close()
+	browser.Close()
+	pw.Stop()
 }
 
-func tryToSolve(host, siteKey string, page playwright.Page) (code string, err error) {
+func tryToSolve(sitekey, host string, page playwright.Page) (code string, err error) {
 	userAgent := uarand.GetRandom()
 
-	hsw, original, err := getHsw(host, siteKey, userAgent, page)
+	hsw, err := generateHsw(host, sitekey, page)
 	if err != nil {
 		return "", err
 	}
 
 	timestamp := makeTimestamp() + randomFromRange(30, 120)
-	movements, err := getMouseMovements(timestamp, 90)
+	movements, err := getMouseMovements(timestamp)
 	if err != nil {
 		return "", err
 	}
 
-	var newViewport NewViewport
-	newViewport.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-	newViewport.ProductSub = "20030107"
-	newViewport.Vendor = "Google Inc."
-	newViewport.GlobalPrivacyControl = true
-	newViewport.HardwareConcurrency = 10
-	newViewport.CookieEnabled = true
-	newViewport.AppCodeName = "Mozilla"
-	newViewport.AppName = "Netscape"
-	newViewport.Platform = "Win32"
-	newViewport.Product = "Gecko"
-	newViewport.AppVersion = "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-	newViewport.Language = "en-US"
-	newViewport.Languages = append(newViewport.Languages, "en-US")
-	newViewport.Languages = append(newViewport.Languages, "en")
-	newViewport.Online = true
-	newViewport.DeviceMemory = 4
-	newViewport.Plugins = []string{}
-
-	var screen Screen
-	screen.AvailableWidth = 1920
-	screen.AvailableHeight = 1040
-	screen.Width = 1920
-	screen.Height = 1080
-	screen.ColorDepth = 24
-	screen.PixelDepth = 24
-
-	timestamp += randomFromRange(30, 120)
-
-	var topLevel TopLevel
-	topLevel.NewViewport = newViewport
-	topLevel.Version = 1
-	topLevel.Start = timestamp
-	topLevel.Screen = screen
-	topLevel.Direct = "https://minecraftpocket-servers.com/server/80103/"
-	topLevel.XY, err = getXYMovements(timestamp)
-	if err != nil {
-		return "", err
-	}
-
-	timestamp += randomFromRange(30, 120)
-
-	topLevel.WN = generateWN(timestamp)
-
-	timestamp += randomFromRange(30, 120)
-
-	topLevel.Movements, err = getMouseMovements(timestamp, 472)
-	if err != nil {
-		return "", err
-	}
-
-	var motionData MotionData
-	motionData.Version = 1
-	motionData.Start = timestamp
-	motionData.Movements = movements
-	motionData.Md = append(motionData.Md, makeMovement(makeTimestamp()+randomFromRange(30, 120)))
-	motionData.Mu = append(motionData.Mu, makeMovement(makeTimestamp()+randomFromRange(30, 120)))
-	motionData.TopLevel = topLevel
-	motionData.Session = []string{}
-	motionData.WidgetList = append(motionData.WidgetList, "07r95qrrtxj")
-	motionData.WidgetId = "07r95qrrtxj"
-	motionData.Href = "https://minecraftpocket-servers.com/server/80103/vote/"
-
-	values, err := query.Values(motionData)
-	if err != nil {
-		return "", err
-	}
+	motionData := url.Values{}
+	motionData.Add("st", strconv.Itoa(int(timestamp)))
+	motionData.Add("dct", strconv.Itoa(int(timestamp)))
+	motionData.Add("mm", movements)
 
 	form := url.Values{}
-	form.Add("siteKey", siteKey)
+	form.Add("sitekey", sitekey)
 	form.Add("host", host)
-	form.Add("motionData", values.Encode())
-	form.Add("n", hsw)
-	form.Add("c", original)
-	form.Add("v", "6043b6da")
+	form.Add("n", hsw.N)
+	form.Add("c", hsw.C)
+	form.Add("motionData", motionData.Encode())
 
 	req, err := http.NewRequest("POST", "https://hcaptcha.com/getcaptcha", strings.NewReader(form.Encode()))
 	if err != nil {
 		return "", err
 	}
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Origin", "https://assets.hcaptcha.com")
-	req.Header.Set("Referer", "https://assets.hcaptcha.com/")
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("User-Agent", userAgent)
 
@@ -149,8 +94,7 @@ func tryToSolve(host, siteKey string, page playwright.Page) (code string, err er
 	if err != nil {
 		return "", err
 	}
-	response := gjson.ParseBytes(b)
-	fmt.Println(string(b))
+	response := gjson.Parse(string(b))
 	if response.Get("generated_pass_UUID").Exists() {
 		return response.Get("generated_pass_UUID").String(), nil
 	}
@@ -161,58 +105,76 @@ func tryToSolve(host, siteKey string, page playwright.Page) (code string, err er
 		return "", err
 	}
 
-	//key := response.Get("key").String()
-	//job := response.Get("request_type").String()
+	key := response.Get("key").String()
+	job := response.Get("request_type").String()
 
-	taskResponses := url.Values{}
+	taskResponses := make(map[string][]string)
 	for _, t := range tasks {
-		taskResponses.Add(t.Key, randomTrueFalse())
+		taskResponses[t.Key] = []string{randomTrueFalse()}
+	}
+
+	timestamp = makeTimestamp() + randomFromRange(30, 120)
+	movements, err = getMouseMovements(timestamp)
+	if err != nil {
+		return "", err
+	}
+
+	motionData = url.Values{}
+	motionData.Add("st", strconv.Itoa(int(timestamp)))
+	motionData.Add("dct", strconv.Itoa(int(timestamp)))
+	motionData.Add("mm", movements)
+
+	form = url.Values{}
+	form.Add("answers", url.Values(taskResponses).Encode())
+	form.Add("sitekey", sitekey)
+	form.Add("serverdomain", host)
+	form.Add("job_mode", job)
+	form.Add("motionData", motionData.Encode())
+
+	req, err = http.NewRequest("POST", "https://hcaptcha.com/checkcaptcha/"+key, strings.NewReader(form.Encode()))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("User-Agent", userAgent)
+
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	b, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	response = gjson.Parse(string(b))
+	if response.Get("generated_pass_UUID").Exists() {
+		return response.Get("generated_pass_UUID").String(), nil
 	}
 
 	return "", errors.New(string(b))
 }
 
 func randomTrueFalse() string {
-	return strconv.FormatBool(rand.Intn(2) == 1)
+	return strconv.FormatBool(sRand.Intn(2) == 1)
 }
 
-func getXYMovements(timestamp int64) (mouseMovements [][]float64, err error) {
-	lastMovement := timestamp
-	var current float64
-	for i := 0; i < 67; i++ {
-		lastMovement += randomFromRange(0, 10)
-		mouseMovements = append(mouseMovements, []float64{0, current, 0.9745889387144993, float64(timestamp)})
-		current += randomFloatFromRange(0, 5)
+func getMouseMovements(timestamp int64) (string, error) {
+	motionCount := randomFromRange(1000, 10000)
+	var mouseMovements [][3]int64
+	for i := 0; i < int(motionCount); i++ {
+		timestamp += randomFromRange(0, 10)
+		mouseMovements = append(mouseMovements, [3]int64{randomFromRange(0, 600), randomFromRange(0, 600), timestamp})
 	}
-
-	return
-}
-
-func getMouseMovements(timestamp int64, movementAmount int) (mouseMovements [][]int64, err error) {
-	lastMovement := timestamp
-	for i := 0; i < movementAmount; i++ {
-		lastMovement += randomFromRange(0, 10)
-		mouseMovements = append(mouseMovements, makeMovement(lastMovement))
+	b, err := json.Marshal(mouseMovements)
+	if err != nil {
+		return "", err
 	}
-
-	return
-}
-
-func generateWN(timestamp int64) [][]int64 {
-	// TODO: Figure out WTF these values are and stop hardcoding them
-	return [][]int64{{652, 973, 1, timestamp}}
-}
-
-func makeMovement(timestamp int64) []int64 {
-	return []int64{randomFromRange(0, 500), randomFromRange(0, 500), timestamp}
-}
-
-func randomFloatFromRange(min, max int) float64 {
-	return float64(rand.Intn(max-min) + min)
+	return string(b), nil
 }
 
 func randomFromRange(min, max int) int64 {
-	return int64(rand.Intn(max-min) + min)
+	return int64(sRand.Intn(max-min) + min)
 }
 
 func makeTimestamp() int64 {

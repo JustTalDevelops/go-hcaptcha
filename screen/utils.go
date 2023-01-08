acb9f17274f7482b940b5293162ea4052a7f7dfa
JustTal <justtal@protonmail.com>
2023-01-07 22:01:23 -0600

    screen/utils.go: Fix extra incorrect knots.

--- screen/utils.go
+++ screen/utils.go
@@ -20,7 +20,7 @@ func merge(a, b []int) []mgl64.Vec2 {
 
 // knots generates a random choice of knots based on the size provided.
 func knots(firstBoundary, secondBoundary, size int) []int {
-	result := make([]int, size)
+	result := make([]int, 0, size)
 	for i := 0; i < size; i++ {
 		result = append(result, utils.Between(firstBoundary, secondBoundary))
 	}
